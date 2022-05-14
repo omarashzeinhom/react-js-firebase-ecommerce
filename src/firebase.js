006cf40745c38f868b4087081e47372feb14fce3
omarashzeinhom <omar.ashraf.zeinhom@Hotmail.com>
2022-05-14 15:28:00 +0200

    updated-header

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,4 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseapp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+export {db, auth};
